d2705f29fca3a55101af4cf0c77cd64d7a17c05c
Stephen Flores <sf314@icloud.com>
2019-07-10 00:40:03 -0500

    Add UserInfo resource controller
    This controller will handle access to user-level info, including
    list of users, and list of accounts for a user

---
+++ src/main/java/jfinance/resource/UserInfo.java
@@ -0,0 +1,50 @@
+package jfinance.resource;
+
+/**
+ * UserInfo: RestController
+ * Define endpoints for accessing user-level data
+ * - list users
+ * - list accounts for user
+ */
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping; // For endpoints
+import org.springframework.web.bind.annotation.RequestParam; // For uri params
+import org.springframework.web.bind.annotation.RestController; // For class
+
+import java.util.ArrayList;
+
+@RestController
+public class UserInfo {
+    
+    // ***** Vars 
+    
+    // ***** Index endpoint for API Health check
+    @RequestMapping("/") 
+    public ResponseEntity<?> index() {
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+    
+    // ***** Users endpoint for listing current users
+    @RequestMapping(
+        value = "/users",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<?> getUsers() {
+        ArrayList<String> userList = new ArrayList<String>();
+        
+        // TODO: Business/data logic here
+        userList.add("bob");
+        userList.add("steve");
+        
+        if (userList.size() == 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        
+        return new ResponseEntity<ArrayList<String>>(userList, HttpStatus.OK);
+    }
+    
+    
+}
